0acd2109b3aa52f0bbe4dc83f60b091d594ef824
Samarth Pujari <samarthpujari249@gmail.com>
2024-06-12 11:31:28 +0530

    Modified

--- lists_ips.py
+++ lists_ips.py
@@ -1,3 +1,14 @@
+# Consider a list (list = []). You can perform the following commands:
+
+# insert i e: Insert integer e at position i.
+# print: Print the list.
+# remove e: Delete the first occurrence of integer e.
+# append e: Insert integer e at the end of the list.
+# sort: Sort the list.
+# pop: Pop the last element from the list.
+# reverse: Reverse the list.
+# Initialize your list and read in the value of  followed by n lines of commands where each command will be of the 7 types listed above. Iterate through each command in order and perform the corresponding operation on your list.
+
 if __name__ == '__main__':
     N = int(input())
 
