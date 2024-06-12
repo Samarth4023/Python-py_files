49a0b85a3c541f62911847798c08ecf88cc5765b
Samarth Pujari <samarthpujari249@gmail.com>
2024-06-12 11:29:12 +0530

    Modified

--- tuples_hashvalue.py
+++ tuples_hashvalue.py
@@ -1,3 +1,7 @@
+# Given an integer, n, and n space-separated integers as input, create a tuple, , of those  integers. Then compute and print the result of hash(i).
+
+# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
+
 if __name__ == '__main__':
     n = int(input())
     integer_list = map(int, input().split())
