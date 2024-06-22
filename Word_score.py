c6c19b134e9ec8bf9e6e6a54d59d4c433e329b86
Samarth Pujari <samarthpujari249@gmail.com>
2024-06-22 23:03:49 +0530

    New File

---
+++ Word_score.py
@@ -0,0 +1,7 @@
+# Consider that vowels in the alphabet are a, e, i, o, u and y.
+# Function score_words takes a list of lowercase words as an argument and returns a score as follows:
+# The score of a single word is  if the word contains an even number of vowels. Otherwise, the score of this word is . The score for the whole list of words is the sum of scores of all words in the list.
+# Debug the given function score_words such that it returns a correct score.
+# Your function will be tested on several cases by the locked template code.
+# Input Format
+# The input is read by the provided locked code template. In the first line, there is a single integer  denoting the number of words. In the second line, there are  space-separated lowercase words.rcase words.rcase words.
